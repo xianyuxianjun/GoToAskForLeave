4e386402fdec44c2a05e2f23f91a9cb52b0ee7fa
yzx <642273990@qq.com>
2024-11-25 23:05:05 +0800

    提供了一个接口，允许客户端通过提供课程信息来删除数据库中的课程记录，从而实现课程管理功能。

--- src/main/java/com/chenxianyu/controller/ClassesController.java
+++ src/main/java/com/chenxianyu/controller/ClassesController.java
@@ -49,6 +49,7 @@ public class ClassesController {
     }
 
     @PostMapping("/delectClasses")
+    // 调用服务层的方法，根据课程对象删除课程信息并返回结果
     public Reslut delectClasses(@RequestBody Classes classes){
         return classesService.delectClasses(classes);
     }
