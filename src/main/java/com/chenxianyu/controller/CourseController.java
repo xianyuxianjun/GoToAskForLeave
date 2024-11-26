cb5d2455a2f07321d3ab222729f9905c36db5f30
yzx <642273990@qq.com>
2024-11-26 18:42:07 +0800

    delectCourse删除课程。 updateCourse更新课程信息。 getCourse获取特定学生所报的课程。 getAllCourse获取所有课程的信息。

--- src/main/java/com/chenxianyu/controller/CourseController.java
+++ src/main/java/com/chenxianyu/controller/CourseController.java
@@ -28,23 +28,46 @@ public class CourseController {
     // 调用服务层的方法，获取课程列表，并返回结果
         return courseService.getCourseList(insrtructor);
     }
-
+    /**
+     * 处理 POST 请求，删除指定的课程。
+     *
+     * @param course 包含要删除的课程信息的 Course 对象。
+     * @return 返回一个 Reslut 对象，指示删除操作的结果，包括成功或失败状态及相关信息。
+     */
     @PostMapping("/delectCourse")
+    // 调用服务层的方法，删除课程并返回结果
     public Reslut delectCourse(@RequestBody Course course){
         return courseService.delectCourse(course);
     }
-
+    /**
+     * 处理 POST 请求，更新指定的课程信息。
+     *
+     * @param course 包含更新后的课程信息的 Course 对象。
+     * @return 返回一个 Reslut 对象，指示更新操作的结果，包括成功或失败状态及相关信息。
+     */
     @PostMapping("/updateCourse")
+    // 调用服务层的方法，更新课程信息并返回结果
     public Reslut updateCourse(@RequestBody Course course){
         return courseService.updateCourse(course);
     }
-
+    /**
+     * 处理 POST 请求，获取指定学生报名的课程信息。
+     *
+     * @param student 包含学生身份信息的 Student 对象，用于查询该学生所报课程。
+     * @return 返回一个 Reslut 对象，包含指定学生的课程信息。
+     */
     @PostMapping("/getCourse")
+    // 调用服务层的方法，获取学生所报的课程并返回结果
     public Reslut getCourse(@RequestBody Student student){
         return courseService.getCourse(student);
     }
-
+    /**
+     * 处理 POST 请求，获取所有课程的列表。
+     *
+     * @return 返回一个 Reslut 对象，包含所有课程的信息和相关状态。
+     */
     @PostMapping("/getAllCourse")
+    // 调用服务层的方法，获取所有课程信息并返回结果
     public Reslut getAllCourse(){
         return courseService.getAllCourse();
     }
