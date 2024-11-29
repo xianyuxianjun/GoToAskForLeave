4d59638cd56ef2b983e3c2ed41ca48cf0181e6a3
yzx <642273990@qq.com>
2024-11-29 22:35:54 +0800

    这段代码是一个Spring Boot中的RestController，用于处理注册请求。其中包括一个POST请求映射到"/api/register"路径的方法Register，该方法接收一个RegisterDto对象作为请求体，并调用RegisterService的segister方法进行注册操作，最终返回注册结果Reslut对象。同时，通过注解@CrossOrigin实现跨域请求的支持。

--- src/main/java/com/chenxianyu/controller/RegisterController.java
+++ src/main/java/com/chenxianyu/controller/RegisterController.java
@@ -1,5 +1,3 @@
-package com.chenxianyu.controller;
-
 import com.chenxianyu.model.dto.RegisterDto;
 import com.chenxianyu.model.vo.Reslut;
 import com.chenxianyu.service.RegisterService;
@@ -10,11 +8,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class RegisterController {
+
     @Autowired
     private RegisterService registerService;
 
+    /**
+     * 处理注册请求
+     * @param registerDto 注册信息
+     * @return 注册结果
+     */
     @PostMapping("/register")
     public Reslut Register(@RequestBody RegisterDto registerDto){
+        // 调用注册服务
         return registerService.segister(registerDto);
     }
 }
