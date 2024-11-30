f971f71edb36caebe3dd42918ea15b6606255264
yzx <642273990@qq.com>
2024-11-30 14:42:52 +0800

    接收一个LoginDto对象作为请求体，并调用LoginService的login方法进行登录操作，最终返回登录结果Reslut对象。

--- src/main/java/com/chenxianyu/controller/LoginContorller.java
+++ src/main/java/com/chenxianyu/controller/LoginContorller.java
@@ -6,12 +6,20 @@ import com.chenxianyu.model.vo.Reslut;
 import com.chenxianyu.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class LoginContorller {
+
     @Autowired
     private LoginService loginService;
+
+    /**
+     * 处理登录请求
+     * @param loginDto 登录信息
+     * @return 登录结果
+     */
     @ResponseBody
     @PostMapping("/login")
     public Reslut login(@RequestBody LoginDto loginDto){
